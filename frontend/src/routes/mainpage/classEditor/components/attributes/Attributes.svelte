7d221b2d72aaf83abffbbef1d5cebd97bcb4a61a
Philipp <philipp.kirchner@soptim.de>
2026-03-19 13:21:40 +0100

    better class editor loading animation

--- frontend/src/routes/mainpage/classEditor/components/attributes/Attributes.svelte
+++ frontend/src/routes/mainpage/classEditor/components/attributes/Attributes.svelte
@@ -16,17 +16,19 @@
   -->
 <script>
     import { faPlus } from "@fortawesome/free-solid-svg-icons";
-    import { getContext } from "svelte";
+    import { getContext, onMount } from "svelte";
 
     import FaIconButton from "$lib/components/FaIconButton.svelte";
     import List from "$lib/components/List.svelte";
+    import { editorState } from "$lib/sharedState.svelte.js";
 
     import Attribute from "./Attribute.svelte";
     import AttributeEditorDialog from "./AttributeEditorDialog.svelte";
 
     const { attributes, openPropertySHACLRulesDialog } = $props();
 
-    const readonly = getContext("classEditor").readonly;
+    const classEditorContext = getContext("classEditor");
+    let readonly = $state(false);
 
     const attributeEditorDialog = $state({
         showDialog: false,
@@ -34,6 +36,13 @@
     });
 
     let expandStereotypes = $state(true);
+
+    $effect(() => {
+        editorState.selectedPackageUUID.subscribe();
+        readonly = getContext("classEditor").readonly;
+    });
+
+    onMount(() => (readonly = classEditorContext.readonly));
 
     function openAttributeEditor(attribute) {
         attributeEditorDialog.attribute = attribute;
