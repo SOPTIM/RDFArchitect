1c0f70f9b5f9ed2d9ceb6587348996218ca15f3e
Philipp <philipp.kirchner@soptim.de>
2026-04-16 11:30:42 +0200

    renamed ontology to profile header meta data

--- frontend/src/lib/GraphExport.svelte
+++ frontend/src/lib/GraphExport.svelte
@@ -233,12 +233,14 @@
                 </div>
             {:else}
                 <p class="mt-2 text-sm italic">
-                    No ontology entries can be generated for the selected
-                    schema.
+                    No profile header meta data entries can be generated for the
+                    selected schema.
                 </p>
             {/if}
         {:else if graphURI}
-            <p class="mt-2 text-sm italic">This schema has no ontology</p>
+            <p class="mt-2 text-sm italic">
+                This schema has no profile header meta data
+            </p>
         {/if}
     {/if}
     <label for="media-types-Download" class="mt-2 mb-1">Media type</label>
