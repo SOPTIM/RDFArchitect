0a16e92eef9349af456ded053ccf6c9bc07d37e7
Prabhpreet Dua <615318+prabhpreet@users.noreply.github.com>
2026-05-12 17:36:30 +0530

    feat: Fixes for run with docker compose
    Signed-off-by: Prabhpreet Dua <615318+prabhpreet@users.noreply.github.com>

--- backend/src/main/java/org/rdfarchitect/config/WebConfig.java
+++ backend/src/main/java/org/rdfarchitect/config/WebConfig.java
@@ -29,10 +29,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final FrontendConfig frontendConfig;
+
+    public WebConfig(FrontendConfig frontendConfig) {
+        this.frontendConfig = frontendConfig;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:1407")
+                .allowedOrigins(frontendConfig.getUrl())
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true)
