4ed8e2028ad30981acc0ee8240487e026c2d34f4
Philipp <philipp.kirchner@soptim.de>
2026-09-03 10:14:36 +0200

    fix

--- frontend/src/routes/mainpage/classEditor/components/attributes/AttributeEditorDialog.svelte
+++ frontend/src/routes/mainpage/classEditor/components/attributes/AttributeEditorDialog.svelte
@@ -27,6 +27,7 @@
     import { mapReactiveAttributeToAttributeDto } from "$lib/models/reactive/mapper/map-reactive-object-to-dto.js";
     import { ReactiveAttribute } from "$lib/models/reactive/models/reactive-attribute.svelte.js";
     import { getControlButtonsForReactiveObject } from "$lib/models/reactive/utils/reactive-objects-control-button-utils.js";
+    import { ATTRIBUTE_STEREOTYPE } from "$lib/models/stereotype-constants.js";
     import { forceReloadTrigger } from "$lib/sharedState.svelte.js";
     import { getNsPrefixNsUriString } from "$lib/utils/namespace.js";
 
@@ -57,12 +58,12 @@
             isNewAttribute = true;
             attribute = new ReactiveAttribute({
                 namespace: classEditorContext.reactiveClass.namespace.value,
+                stereotypes: [ATTRIBUTE_STEREOTYPE],
             });
             attributes.appendClass(attribute);
         } else {
             isNewAttribute = false;
         }
-        console.log({ attribute });
     }
 
     function onClose() {
